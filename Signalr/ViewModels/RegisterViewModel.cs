791c61d78c9624d8fb660c0a995d90e3ad9f0a44
aya <aya@gmail.com>
2024-09-24 10:39:36 -0700

    edit2

--- Signalr/ViewModels/RegisterViewModel.cs
+++ Signalr/ViewModels/RegisterViewModel.cs
@@ -6,14 +6,14 @@ namespace SignalR.ViewModels
     {
         [Required]
         [EmailAddress]
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
 
         [Required]
         [DataType(DataType.Password)]
-        public string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
 
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
+        public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
